ef1e2c657380a0cb73f518703a9dad3b9774c230
Kristopher Chambers <kristopher.chambers@gmail.com>
2013-07-06 13:06:39 -0700

    Getting the subdomain from the referer, still support param method. Refactoring.

--- application.rb
+++ application.rb
@@ -6,20 +6,20 @@ require 'redis'
 require './config/redis.rb'
 
 class Entry
-  attr_accessor :key, :name, :message, :created_at
+  attr_accessor :subdomain, :name, :message, :created_at
 
   def self.per_page
     10
   end
 
-  def self.get(key, page = 1)
+  def self.get(subdomain, page = 1)
     start =
       if page <= 1
         0
       else
         ((page - 1) * 10) - 1
       end
-    Redis.current.lrange(key, start, start + per_page)
+    Redis.current.lrange(subdomain, start, start + per_page)
   end
 
   def initialize(attrs)
@@ -30,12 +30,12 @@ class Entry
   end
 
   def valid?
-    if !key.nil? && !key.empty?
-      if key.length > 36
-        errors['key'] = 'Cannot be more than 36 characters'
+    if !subdomain.nil? && !subdomain.empty?
+      if subdomain.length > 255
+        errors['subdomain'] = 'Cannot be more than 255 characters'
       end
     else
-      errors['key'] = 'Cannot be blank'
+      errors['subdomain'] = 'Cannot be blank'
     end
     if !name.nil? && !name.empty?
       if name.length > 100
@@ -59,7 +59,7 @@ class Entry
   end
 
   def save
-    Redis.current.lpush(key, as_json.to_json)
+    Redis.current.lpush(subdomain, as_json.to_json)
   end
 
   def as_json
@@ -81,34 +81,58 @@ class Application
     @request = Rack::Request.new(env)
   end
 
+  def not_found
+    [404, {}, []]
+  end
+
   def params
     @request.params
   end
 
-  def headers
-    @headers = {
-      'Content-Type' => 'application/json'
-    }
+  def referer
+    @request.referer || @env['HTTP_REFERER']
+  end
+
+  def subdomain
+    @subdomain ||=
+      if referer && (matches = referer.scan(/http(s)?\:\/\/(.+)\.neocities\.org/i)[0]) && matches[1]
+        matches[1]
+      elsif !params['subdomain'].nil? && !params['subdomain'].empty?
+        params['subdomain']
+      elsif !params['key'].nil? && !params['key'].empty?
+        params['key']
+      end
+  end
+
+  def with_callback(body)
+    if params['callback']
+      params['callback'] + "(#{body});"
+    else
+      body
+    end
   end
 
   def dispatch
+    unless subdomain
+      return not_found
+    end
+
+    headers ||= {
+      'Content-Type' => 'application/json'
+    }
+
     if @env['HTTP_ORIGIN'] && @env['HTTP_ORIGIN'] =~ /http(s)?\:\/\/.+\.neocities\.org/i
       headers['Access-Control-Allow-Origin'] = @env['HTTP_ORIGIN']
     end
 
-    puts @env
-    puts @env['HTTP_ORIGIN']
-    puts headers
-
     if @request.get?
-      body = "[" + Entry.get(params['key'], (params['page'] || 1).to_i).join(',') + "]"
-      if params['callback']
-        body = params['callback'] + "(#{body});"
-      end
-      [200, headers, [body]]
+      [200, headers, [
+          with_callback("[" + Entry.get(subdomain, (params['page'] || 1).to_i).join(',') + "]")
+        ]
+      ]
     elsif @request.post?
       @entry = Entry.new({
-        key: params['key'],
+        subdomain: subdomain,
         name: params['name'],
         message: params['message'],
       })
@@ -117,18 +141,10 @@ class Application
         if params['return_to']
           [302, { 'Location' => params['return_to'] }, []]
         else
-          body = @entry.as_json.to_json
-          if params['callback']
-            body = params['callback'] + "(#{body})"
-          end
-          [201, headers, [body]]
+          [201, headers, [with_callback(@entry.as_json.to_json)]]
         end
       else
-        body = { errors: @entry.errors }.to_json
-        if params['callback']
-          body = params['callback'] + "(#{body})"
-        end
-        [422, headers, [body]]
+        [422, headers, [with_callback({ errors: @entry.errors }.to_json)]]
       end
     else
       [404, {}, []]
